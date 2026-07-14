d54d1d4436fa96f72cac8793a33da69ad19a4e1f
rex-del <150361329+rex-del@users.noreply.github.com>
2026-07-15 02:56:35 +0300

    Fix Swift compile errors blocking CI build

--- iOS/CoolantLog/Models.swift
+++ iOS/CoolantLog/Models.swift
@@ -4,6 +4,6 @@ struct FlushEntry: Identifiable, Codable, Equatable {
     var id: UUID = UUID()
     var date: Date = Date()
     var notes: String = ""
-    var coolantType: String
-    var mileage: Double
+    var coolantType: String = ""
+    var mileage: Double = 0
 }
